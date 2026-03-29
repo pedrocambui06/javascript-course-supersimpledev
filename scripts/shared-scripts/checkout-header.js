c0e84f7433178884d366f9431933991af4a8231e
eupedrocambui <pedrocambui06@gmail.com>
2026-03-28 23:59:27 -0300

    (fix) Removing checkout header logo Removing logos due to copyright problems with github pages

--- scripts/shared-scripts/checkout-header.js
+++ scripts/shared-scripts/checkout-header.js
@@ -3,13 +3,6 @@ import { checkoutHeaderLoaded } from "../events/events.js";
 const checkoutHeaderHTML =
 `
     <div class="header-content">
-        <div class="checkout-header-left-section">
-            <a href="index.html">
-                <img class="amazon-logo" src="images/amazon-logo.png">
-                <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
-            </a>
-        </div>
-
         <div class="checkout-header-middle-section js-checkout-header-middle-section">
             Checkout (<a class="return-to-home-link js-header-cart-quantity js-cart-quantity"
             href="index.html">0 Items</a>)
