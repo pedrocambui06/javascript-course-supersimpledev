ed810d1ecca0b9a31030c242b782142941611139
eupedrocambui <pedrocambui06@gmail.com>
2026-03-28 23:53:34 -0300

    (fix) Removing footer logo removing footer logo due to copyright problems with github pages (warning messages)

--- scripts/shared-scripts/footer.js
+++ scripts/shared-scripts/footer.js
@@ -7,25 +7,25 @@ const footerHTML =
             <div class="info">
                 <h1>Get to Know Us</h1>
                 <div class="links">
-                    <p>About Amazon</p>
-                    <p>Amazon Newsletter</p>
+                    <p>About Us</p>
+                    <p>Newsletter</p>
                     <p>Careers</p>
                 </div>
             </div>
             <div class="info">
                 <h1>Make Money with Us</h1>
                 <div class="links">
-                    <p>Sell on Amazon</p>
+                    <p>Sellers Program</p>
                     <p>Become an Affiliate</p>
                     <p>Protect & Build Your Brand</p>
                 </div>
             </div>
             <div class="info">
-                <h1>Amazon Payment Products</h1>
+                <h1>Payment Products</h1>
                 <div class="links">
-                    <p>Amazon Visa</p>
-                    <p>Amazon Store Card</p>
-                    <p>Amazon Secured Card</p>
+                    <p>Visa</p>
+                    <p>Store Card</p>
+                    <p>Secured Card</p>
                 </div>
             </div>
             <div class="info">
@@ -40,10 +40,7 @@ const footerHTML =
 
         <hr class="footer-line">
 
-        <div class="logo-language">
-            <a href="./index.html">
-                <img class="amazon-logo-footer" src="./images/amazon-logo-white.png" alt="Amazon Logo">
-            </a>
+        <div class="language-container">
             <div class="language">
                 <img class="flag" src="https://flagicons.lipis.dev/flags/4x3/us.svg" alt="US Flag">
                 <p>United States</p>
@@ -52,7 +49,6 @@ const footerHTML =
 
         <div class="legal-info">
             <p>Terms of Service | Privacy Policy | Cookies Policy | Interest-based Ads</p>
-            <p>© 2026 Amazon. All rights reserved.</p>
         </div>
     </div>
 `;
@@ -61,3 +57,10 @@ document.addEventListener('mainLoaded', () => {
     document.querySelector('.amazon-footer').innerHTML = footerHTML;
     footerLoaded();
 });
+
+/* old logo
+
+<a href="./index.html">
+    <img class="amazon-logo-footer" src="./images/amazon-logo-white.png" alt="Logo">
+</a>
+*/
